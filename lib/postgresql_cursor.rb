a78c85bcce5c25a0845c4c6955cc6d45242d1c21
Allen Fair <allen.fair@gmail.com>
2010-05-16 20:19:54 -0400

    got the basics working!

--- lib/postgresql_cursor.rb
+++ lib/postgresql_cursor.rb
@@ -11,28 +11,21 @@ class PostgreSQLCursor
     @instantiator = instantiator
     @sql = sql
     @name = "pgcursor_#{@@counter}"
-    @connection = ActiveRecord.connection
+    @connection = ActiveRecord::Base.connection
     @buffer_size = @options[:buffer_size] || 10_000 
     @count = 0
   end
   
-  # Takes arguments like ActiveRecord#find and returns a cursor object ready to consume
-  def self.find(instantiator, *findargs)
-    options = findargs.last.is_a?(Hash) && findargs.last.delete(:cursor) || {}
-    validate_find_options(:all, *findargs)
-    set_readonly_option!(findargs)
-    sql = construct_finder_sql(:all, *findargs)
-    PostgreSQLAdapter.new(sql, options)
-  end
-  
   # Iterates through the rows, yields them to the block
   def each
-    @result = ActiveRecord.connection.open_cursor(@sql, @options)
-    while (row = fetch_cursor) do
-      yield row
+    @connection.transaction do
+      @result = open_cursor
+      while (row = fetch_cursor) do
+        yield row
+      end
+      close_cursor
+      @count
     end
-    close_cursor(@name)
-    @count
   end
   
   def size
@@ -44,14 +37,14 @@ class PostgreSQLCursor
   # Starts buffered result set processing for a given SQL statement. The DB
   def open_cursor
     @sql = replace_params(*(@sql.flatten)) if @sql.is_a?(Array)
+    #@connection.start_transaction if @connection.outside_transaction?
     @result = @connection.execute("declare #{@name} cursor for #{@sql}")
     @state = :empty
-    @buffer_size = buffer_size
     @buffer = nil
   end
   
   # Fetches the next block of rows into memory
-  def fetch_buffer() #:nodoc:
+  def fetch_buffer #:nodoc:
     return unless @state == :empty
     @result = @connection.execute("fetch #{@buffer_size} from #{@name}")
     @buffer = @result.collect {|row| row }
@@ -61,7 +54,7 @@ class PostgreSQLCursor
 
   # Returns the next row from the cursor, or nil when end of data.
   # The row returned is a hash[:colname]
-  def fetch_cursor()
+  def fetch_cursor
     fetch_buffer if @state == :empty
     return nil   if @state == :eof
     @state = :empty if @buffer.size <= 1
@@ -74,7 +67,7 @@ class PostgreSQLCursor
   alias_method :next, :fetch_cursor
   
   # Closes the cursor to clean up resources
-  def close_cursor(name='csr')
+  def close_cursor
     pg_result = @connection.execute("close #{@name}")
   end
   
@@ -84,18 +77,23 @@ class ActiveRecord::Base
   class <<self
   
     # Returns a PostgreSQLCursor instance to access the results
-    def find_with_cursor(*findopts)
-      puts findopts.inspect
-      options = findopts.last.is_a?(Hash) && findopts.last.delete(:cursor) || {}
-      validate_find_options(findopts)
-      #set_readonly_option!(findopts)
-      sql = construct_finder_sql(findopts)
-      PostgreSQLAdapter.new(lambda {|r| instantiate(r)}, sql, options)
+    def find_with_cursor(*args)
+      find_options = args.last.is_a?(Hash) ? args.pop : {}
+      options = find_options.delete(:cursor) || {}
+      validate_find_options(find_options)
+      set_readonly_option!(find_options)
+      sql = construct_finder_sql(find_options)
+      PostgreSQLCursor.new(lambda {|r| instantiate(r)}, sql, options)
+      #PostgreSQLCursor.new(sql, options)
     end
   
     # Returns a PostgreSQLCursor instance to access the results of the sql
     def find_by_sql_with_cursor(sql, options={})
       PostgreSQLCursor.new(lambda {|r| instantiate(r)}, sql, options)
+    end
+
+    def fetch_cursor(record)
+      instantiate(record)
     end
   
   end
--- test/test_postgresql_cursor.rb
+++ test/test_postgresql_cursor.rb
@@ -3,11 +3,15 @@ require 'helper'
 class TestPostgresqlCursor < Test::Unit::TestCase
   
   def test_load
-    puts List.all.inspect
+    #puts List.all.inspect
   end
   
   def test_cursor
-    c = List.find_with_cursor(:all)
-    puts c.inspect
+    c = List.find_with_cursor(:conditions=>["list_id>?",0])
+    #puts c.inspect
+    c.each do |r| 
+      puts r.inspect 
+      puts r.class
+    end
   end
 end
