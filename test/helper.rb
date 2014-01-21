51255ad15c5c5520ea632a6c80fd30331e892745
DarthSim <darthsim@gmail.com>
2014-01-21 22:10:17 +0700

    test for pluck_each

--- test/helper.rb
+++ test/helper.rb
@@ -11,15 +11,32 @@ class Model < ActiveRecord::Base
   #set_table_name "records"
   self.table_name = "records"
 
+  has_many :children, class_name: "ChildModel", inverse_of: :parent, foreign_key: "parent_id"
+
   # create table records (id serial primary key);
   def self.generate(max=1_000_000)
-    max.times do
-      connection.execute("insert into records values (nextval('records_id_seq'::regclass))")
+    max.times do |i|
+      connection.execute("insert into records values (#{i+1})")
     end
   end
 end
 
-Model.generate(1000) if Model.count == 0
+class ChildModel < ActiveRecord::Base
+  self.table_name = "child_records"
+  belongs_to :parent, class_name: "Model", inverse_of: :children
+
+  def self.generate(max=1_000_000)
+    max.times do |i|
+      connection.execute("insert into child_records values (#{i+1}, 'Lorem#{i}', #{i/2 + 1})")
+    end
+  end
+end
+
+Model.delete_all
+Model.generate(1000)
+
+ChildModel.delete_all
+ChildModel.generate(1000)
 
 class Test::Unit::TestCase
 end
