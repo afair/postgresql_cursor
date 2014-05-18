a939533b358d18448a473072b59eb31d6a9acbde
Allen Fair <allen.fair@gmail.com>
2014-05-18 18:06:05 -0400

    Updates README, removes unneeded files

--- lib/postgresql_cursor/active_record/connection_adapters/postgresql_type_map.rb
+++ lib/postgresql_cursor/active_record/connection_adapters/postgresql_type_map.rb
@@ -3,6 +3,7 @@ module PostgreSQLCursor
   module ActiveRecord
     module ConnectionAdapters
       module PostgreSQLTypeMap
+        # Returns the private "type_map" needed for the cursor operation
         def get_type_map # :nodoc:
           type_map
         end
--- lib/postgresql_cursor/railtie.rb
+++
@@ -1,6 +0,0 @@
-# Placeholder: Probably won't be used
-module PostgreSQLCursor
-  class Railtie < Rails::Railtie
-
-  end
-end
